3e9ad880974608c874a3b0220c495e8c701ea677
Kimyeonho <kyh0703@nate.com>
2025-01-09 23:01:04 +0900

    🎨 [FIBER] INIT APP

--- cmd/layout/app.go
+++ cmd/layout/app.go
@@ -1,8 +1,15 @@
 package main
 
-import "context"
+import (
+	"context"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
 
 type app struct {
+	fiber   *fiber.App
 	perfSvr *PerfServer
 }
 
@@ -12,13 +19,22 @@ func NewApp(perfSvr *PerfServer) *app {
 	}
 }
 
+func (a *app) setupLogger() fiber.Handler {
+	return logger.New(logger.Config{
+		Format:     "${time} ${status} - ${method} ${path}\n",
+		TimeFormat: "02-Jan-2006",
+		TimeZone:   "Asia/Seoul",
+	})
+}
+
 func (a *app) Run(ctx context.Context) error {
-	// Run the application
+	a.fiber = fiber.New()
+	a.fiber.Use(cors.New())
+	a.fiber.Use(a.setupLogger())
 	return nil
 }
 
 func (a *app) Stop(ctx context.Context) error {
-	// Stop the application
 	a.perfSvr.Shutdown(ctx)
 	return nil
 }
